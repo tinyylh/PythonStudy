9695e3152c4aeedc7b371e439fca681c4b0248f9
tinyylh <tinyylh@gmail.com>
2016-01-28 17:03:38 +0800

    fab yield

---
+++ HelloPy/Fab.py
@@ -0,0 +1,15 @@
+# def fab(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         print b
+#         a, b = b, a + b
+#         n = n + 1
+
+def fab(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+
+fab(10)
